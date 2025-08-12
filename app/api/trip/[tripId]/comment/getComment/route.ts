bc0495d51746a895d3d01bd6474e30f828f78d24
UniqueIam <abhimanyukumar83097@gmail.com>
2025-08-12 09:04:19 +0530

    get comments on trip

--- app/api/trip/[tripId]/comment/getComment/route.ts
+++ app/api/trip/[tripId]/comment/getComment/route.ts
@@ -1,5 +1,55 @@
-import { NextRequest,NextResponse } from "next/server";
+import { prisma } from "@/lib/db/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest){
-    return NextResponse.json({message:'Will add soon'})
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { tripId: string } }
+) {
+  try {
+    const { tripId } = params;
+    if (!tripId) {
+      return NextResponse.json(
+        { error: "tripId is required" },
+        { status: 400 }
+      );
+    }
+
+    const tripExists = await prisma.trip.findUnique({
+      where: { id: tripId },
+      select: { id: true },
+    });
+
+    if (!tripExists) {
+      return NextResponse.json({ error: "Trip not found" }, { status: 404 });
+    }
+
+    const comments = await prisma.comment.findMany({
+      where: { tripId },
+      select: {
+        content: true,
+        author: {
+          select: {
+            id: true,
+            profile: {
+              select: {
+                displayName: true,
+                avatarUrl: true,
+              },
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json({ success: true, comments }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
